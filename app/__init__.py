a2ef78de0c598117a10aac824b7fced2e6fcdafe
Spencer Trinh <spencertrinh@Spencers-MacBook-Air.local>
2019-02-14 23:28:20 -0500

    new edits

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 hpath = '/Users/spencertrinh/GitRepos/Flask_chemITry/chemITryFlask'
-app =Flask(__name__,static_folder='/Users/spencertrinh/GitRepos/Flask_chemITry/chemITryFlask/app/static')#,instance_path=hpath + '/protected')
+app =Flask(__name__,static_folder=hpath+'/app/static')#,instance_path=hpath + '/protected')
 app.config.from_object('config')
 db = SQLAlchemy(app)
 from app import views, models
--- app/static/jscripts.js
+++ app/static/jscripts.js
@@ -26,36 +26,42 @@ $(function() {
 
 
 /* LOADING SCREEN */
-$(document).ready(function() {
+$(function() {
+   $('submit-button').click(function() {
   const lockModal = $("#lock-modal");
   const loadingCircle = $("#loading-circle");
   const form = $("#form-subsrch");
 
-  form.on('submit', function(e) {
-    e.preventDefault(); //prevent form from submitting
+   e.preventDefault(); //prevent form from submitting
 
     // lock down the form
     lockModal.css("display", "block");
     loadingCircle.css("display", "block");
-
-    form.children("input").each(function() {
-            $(this).attr("readonly", true);
-            });
     
-    // re-enable the form
-     $(document).ready(function() { 
-       $('#form-subsrch').submit(function(){
-         lockModal.css("display", "none");
-         loadingCircle.css("display", "none");
-      
-       form.children("input").each(function() {
-         $(this).attr("readonly", false);
-        }); 
-     });
-  });
+   $.ajax({
+      url:'/smrtsrch/',
+      data: $('form-subsrch').serialize(),
+      type:'POST',
+      success: function(response) {
+         console.log(response);
+      },
+      error: function(error){
+         console.log(error);
+         }
+       });
+   });
 });
-});
-//function loading(){
+
+
+
+    
+  
+ 
+
+
+
+
+  //function loading(){
   //          $("#loading-circle").show();
     //        $("#lock-modal").hide();       
       //  }
